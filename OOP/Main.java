5af5cdb7ede168e67ae0d59d24cbe5581f557e0d
CuevasLuis <luisc_92@yahoo.com>
2023-10-01 23:04:53 -0500

    constuctor added

--- OOP/Main.java
+++ OOP/Main.java
@@ -6,13 +6,36 @@ public class Main {
 
     String bookName = "";
     int ISBM = 0;
+    int numberOFpages = 0;
     boolean isAvailable = false;
     boolean onSale = false;
+    boolean isDigital = false;
+    boolean isKidFriendly = false;
+    
     // authors firs and last name to search all books by the author 
     String authorFirstName = "";
     String authorLastName = "";
     String genre = "";
+
+
+    public Main(String bookname, int isbm, int pagenumber, boolean available, boolean onsale, boolean digital, boolean kidfriendly, String fname, String lname,
+    String genre){
+    this.bookName = bookname;
+    this.ISBM =  isbm;
+    this.numberOFpages = pagenumber;
+    this.isAvailable = available;
+    this.onSale = onsale;
+    this.isDigital = digital;
+    this.isKidFriendly = kidfriendly;
+    this.authorFirstName = fname;
+    this.authorLastName = lname;
+    this.genre = genre;
     
+
+
+
+        
+    };
 
 
 
