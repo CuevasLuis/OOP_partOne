52c84b1c8ee8404ba63defd6c22d81000157f3c5
CuevasLuis <luisc_92@yahoo.com>
2023-10-01 22:15:01 -0500

    adding variables

---
+++ OOP/Main.java
@@ -0,0 +1,43 @@
+package OOP;
+import java.util.Scanner;
+
+public class Main {
+
+
+    String bookName = "";
+    int ISBM = 0;
+    boolean isAvailable = false;
+    boolean onSale = false;
+    // authors firs and last name to search all books by the author 
+    String authorFirstName = "";
+    String authorLastName = "";
+    String genre = "";
+    
+
+
+
+
+
+
+
+    
+
+    public static void main(String[] args) {
+
+        
+
+
+
+    
+        
+    };
+
+
+
+
+
+
+
+
+
+}
